057c2c499da0567539e2f3df3f5b0ce3282e46bc
JMan <justinvang07@gmail.com>
2021-02-24 17:36:27 -0600

    get workouts logic

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
+const { workout } = require("../models");
 const db = require("../models");
 
-//creat workout
+//get workouts
+router.get("/api/workouts", (req, res) => {
+    db.workout.find({})
+    .then(dbWorkout => {
+        var total = 0;
+        workout.exercises.forEach(e => {
+            total += e.duration;
+        });
+        workout.totalDuration = total;
+    });
+    res.json(dbWorkout);
+}) .catch(err => {
+    res.json(err);
+});
+
+//create workout
 router.post("/api/workouts", ({body}, res) => {
     db.create(body)
     .then(dbWorkout => {

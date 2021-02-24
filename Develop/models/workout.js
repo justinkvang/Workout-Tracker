d36a568b5924a2e3775cfa0fc84cc78884248ea5
JMan <justinvang07@gmail.com>
2021-02-24 16:00:04 -0600

    workout js db set up to create collections

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,18 +2,50 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+// these schema collections are coming from the exercise html
+const WorkoutSchema = new Schema({
   day: {
     type: Date,
     default: Date.now
   },
   exercises: [
     {
-      
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      }
     }
-  ]
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
 });
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = Transaction;
+module.exports = Workout;

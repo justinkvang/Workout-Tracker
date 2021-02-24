0d916df2082d40096c1eb8e21a2ca0d1485fd3e6
JMan <justinvang07@gmail.com>
2021-02-24 17:31:25 -0600

    create workout api code

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
-const Workout = require("../models");
+const db = require("../models");
 
-router.post("/api/workout", ({body}, res) => {
-    Workout.create(body)
+//creat workout
+router.post("/api/workouts", ({body}, res) => {
+    db.create(body)
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
     .catch(err => {
-        res.status(400).json(err);
+        res.json(err);
     });
 });
 

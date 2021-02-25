dc8fbd9ac6731065aac253eb9175c9116b45820e
JMan <justinvang07@gmail.com>
2021-02-25 16:22:29 -0600

    corrected paths for htmls

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect(
   }
 );
 
-app.use(require("./routes/api"));
-app.use(require("./routes/html"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

fad36e01358b126f467de79cd6ae4f05ccb59d38
JMan <justinvang07@gmail.com>
2021-02-25 16:56:01 -0600

    updates

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,7 @@ const db = require("../models");
 
 //get workouts
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-    .then(dbWorkout => {
+    db.Workout.find({}).then(dbWorkout => {
         dbWorkout.forEach(workout => {
         var total = 0;
         workout.exercises.forEach(e => {
@@ -26,9 +25,7 @@ router.put("/api/workouts/:id", (req, res) => {
             $inc: { totalDuration: req.body.duration },
             $push: { exercises: req.body }
         },
-        {new: true}
-    )
-    .then(dbWorkout => {
+        {new: true}).then(dbWorkout => {
         res.json(dbWorkout);
     }) .catch(err => {
         res.json(err);
@@ -36,9 +33,8 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 //create workout
-router.post("/api/workouts", ({body}, res) => {
-    db.Workout.create(body)
-    .then(dbWorkout => {
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then(dbWorkout => {
         res.json(dbWorkout);
     })
     .catch(err => {
@@ -48,8 +44,7 @@ router.post("/api/workouts", ({body}, res) => {
 
 //get workouts in range
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-    .then(dbWorkout => {
+    db.Workout.find({}).then(dbWorkout => {
         res.json(dbWorkout);
     }) . catch(err => {
         res.json(err);
